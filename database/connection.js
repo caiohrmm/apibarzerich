8a6122808708fd99e036dd8c172b45c5f29b2a46
caiohrmm <cahenromar@gmail.com>
2024-08-23 13:00:56 -0300

    Escondendo informacoes privadas

--- database/connection.js
+++ database/connection.js
@@ -3,13 +3,10 @@ const { Sequelize } = require("sequelize");
 // Carregar variáveis de ambiente
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  "mysql://root:kDIagJpTzggHophsyvMeMNsKrvRYgxSQ@junction.proxy.rlwy.net:41776/railway",
-  {
-    dialect: "mysql",
-    timezone: "-03:00",
-  }
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  timezone: "-03:00",
+});
 
 try {
   sequelize.authenticate();
