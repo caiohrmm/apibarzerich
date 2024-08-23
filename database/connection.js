efa6715c569f2659c5b4f1f7cf566b236a37fe14
caiohrmm <cahenromar@gmail.com>
2024-08-23 12:44:48 -0300

    Url de conexao

--- database/connection.js
+++ database/connection.js
@@ -3,10 +3,13 @@ const { Sequelize } = require("sequelize");
 // Carregar variáveis de ambiente
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: "mysql",
-  timezone: "-03:00",
-});
+const sequelize = new Sequelize(
+  "mysql://root:kDIagJpTzggHophsyvMeMNsKrvRYgxSQ@junction.proxy.rlwy.net:41776/railway",
+  {
+    dialect: "mysql",
+    timezone: "-03:00",
+  }
+);
 
 try {
   sequelize.authenticate();
